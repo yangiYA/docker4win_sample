8f120862f5d239738e6a351561394d9485fb2ce7
yangiYA <dummy@dummy.jp>
2016-08-16 00:41:55 +0900

    java & maven  バッチ起動のシェル追加

---
+++ apsv_db_smtp_java/docker-mvn.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+HERE=$(cd $(dirname $0);pwd)
+
+WORK_DIR=${HERE}/$1
+shift
+ARGS=$@
+
+# DOCKER_IMAGE=maven:3-jdk-8
+
+DOCKER_IMAGE=maven:3.3.9-jdk-8
+DCOKER_NAME=docker-mavne
+DOCKER_WORK_DIR=/usr/src/mymaven
+
+echo -e "\n"
+echo -e "****************************************************************************"
+echo -e "WORK_DIR      =${WORK_DIR}"
+echo -e "ARGS          =${ARGS}"
+echo -e "DOCKER_IMAGE  =${DOCKER_IMAGE}"
+echo -e "****************************************************************************"
+echo -e "\n"
+
+docker run -it --rm --name ${DCOKER_NAME} \
+           -v ${WORK_DIR}:${DOCKER_WORK_DIR} \-w ${DOCKER_WORK_DIR} \
+           ${DOCKER_IMAGE} \
+       mvn $ARGS
